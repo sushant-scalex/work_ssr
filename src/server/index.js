93d96f1773afaf27282de33a6483004c3f0e82cd
sushant-scalex <sushant@scalextechnologies.com>
2018-08-09 10:12:45 +0530

    Bundle.js linked and Hello world displayed

--- src/server/index.js
+++ src/server/index.js
@@ -2,16 +2,30 @@ import React from 'react'
 import { renderToString} from 'react-dom/server'
 import express from 'express'
 import App from '../shared/App'
+import cors from "cors"
 let port= 8080;
 
-let app=express();
+let app=express()
+app.use(cors())
+app.use(express.static("public"))
 
 app.get('*',(req,res,next)=>{
     const markup = renderToString(
         <App />
     );
     res.send(`
-        ${markup}
+        <html>
+            <head>
+                <title>
+                    React App with SSR
+                </title>
+                <script src='/bundle.js' defer></script>
+            </head>
+            <body>
+            ${markup}
+            </body>
+        </html>
+        
     `);
 })
 app.listen(port,(req,res)=>{
